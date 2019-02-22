11b1e4a9830e7863ee7002cab7c880ed0007d8aa
Juan Palacios Galván <juanantonio.palacios@intelygenz.com>
2019-02-22 10:51:31 +0100

    Added simple form

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,5 +4,21 @@ app.controller('MainController', [
 
   function($scope, suggestions){
     $scope.suggestions = suggestions;
+
+    $scope.addSuggestion = function(){
+      //if input empty, don't submit
+      if(!$scope.title || $scope.title === "") {
+        return;
+      }
+      
+      //push suggestion posts in suggestions.js
+      $scope.suggestions.push({
+        title: $scope.title,
+        upvotes: 0,
+      });
+      
+      //after submit, clear input
+      $scope.title = '';
+    };
   }
 ]);
