4c4b6f421f2c5bc80761dfe3c8dce18e473baa61
Juan Palacios Galván <juanantonio.palacios@intelygenz.com>
2019-02-22 11:51:30 +0100

    Added routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('SuggestionBoxApp', []);
+var app = angular.module('SuggestionBoxApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    controller: 'MainController',
+    templateUrl: 'views/main.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
